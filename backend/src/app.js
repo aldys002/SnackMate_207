34d9a3ca3057e44001a7b47918acd69cdbf59d0c
aldys002 <igidiaaldys@gmail.com>
2026-01-03 22:59:21 +0700

    membuat endpoint agar bisa mengambil semua data

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
-const path = require("path");
-
+const cors = require("cors");
+const path = require("path"); // biar path static lebih aman
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-app.use(
-  "/images",
-  express.static(path.join(__dirname, "../public/images"))
-);
+// Static folder untuk gambar
+app.use("/images", express.static(path.join(__dirname, "../public/images")));
 
+// Routes
 app.use("/auth", require("./routes/auth.routes"));
-app.use("/apikey", require("./routes/apikey.routes"));
-app.use("/api/snacks", require("./routes/snack.routes"));
 app.use("/admin", require("./routes/admin.routes"));
+
+// Tambah route API Key
+app.use("/apikey", require("./routes/apikey.routes"));
+
+// Tambah route snack (misal nanti ada endpoint /api/snacks)
+app.use("/api/snacks", require("./routes/snack.routes"));
 
 module.exports = app;
