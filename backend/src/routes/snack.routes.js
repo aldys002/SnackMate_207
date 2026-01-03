b9f98f926ffd352a425cb8d8fb5f203918f14296
aldys002 <igidiaaldys@gmail.com>
2026-01-03 23:01:09 +0700

    endpoint snack

--- backend/src/routes/snack.routes.js
+++ backend/src/routes/snack.routes.js
@@ -3,7 +3,19 @@ const router = express.Router();
 const apiKeyMiddleware = require("../middleware/apikey.middleware");
 const snackController = require("../controllers/snack.controller");
 
+// GET all snacks
 router.get("/", apiKeyMiddleware, snackController.getAllSnacks);
-router.get("/:id", apiKeyMiddleware, snackController.getSnackDetail);
+
+// GET snack by ID
+router.get("/:id", apiKeyMiddleware, snackController.getSnackById);
+
+// ADD snack
+router.post("/", apiKeyMiddleware, snackController.addSnack);
+
+// UPDATE snack
+router.put("/:id", apiKeyMiddleware, snackController.updateSnack);
+
+// DELETE snack
+router.delete("/:id", apiKeyMiddleware, snackController.deleteSnack);
 
 module.exports = router;
